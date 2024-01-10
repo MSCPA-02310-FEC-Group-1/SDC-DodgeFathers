309f110cfab43e03fcb644f13945f5f212f2adbc
Thiem Huynh <thiem.huynh0@gmail.com>
2024-01-10 17:21:56 -0500

    modified seedfiles and db migration

--- server/db/migration.sql
+++ server/db/migration.sql
@@ -7,7 +7,7 @@ CREATE TABLE club (
     price DECIMAL(10, 2) NOT NULL,
     sale_price DECIMAL(10, 2) NOT NULL,
     description VARCHAR NOT NULL,
-    hand BOOLEAN NOT NULL,
+    left_hand BOOLEAN NOT NULL,
     loft JSON NOT NULL,
     custom_options JSON NOT NULL
 )
